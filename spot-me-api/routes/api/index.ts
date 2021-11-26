ac845abcd6b938521d693184185e351423fe5ed9
Moiz Ahmad <moiznahmad@gmail.com>
2021-11-26 14:57:18 -0800

    removed auth tester functions

--- spot-me-api/routes/api/index.ts
+++ spot-me-api/routes/api/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import asyncHandler from 'express-async-handler';
-import { setTokenCookie } from '../../utils/auth';
+import { setTokenCookie, restoreUser, requireAuth } from '../../utils/auth';
 import db from '../../db/models'
 import { DefScopeUser } from '../../db/models/user';
 
@@ -16,5 +16,13 @@ router.get('/set-token-cookie', asyncHandler(async (req: any, res: any) => {
     setTokenCookie(res, user);
     return res.json({ user });
 }))
+
+router.get('/restore-user', restoreUser, (req: any, res: any) => {
+    return res.json(req.user);
+})
+
+router.get('/require-auth', requireAuth, (req: any, res: any) => {
+    return res.json(req.user);
+})
 
 export default router;
