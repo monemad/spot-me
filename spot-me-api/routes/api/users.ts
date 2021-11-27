7e3a2a5a88c515a6cb28a152f542f72177721661
Moiz Ahmad <moiznahmad@gmail.com>
2021-11-26 17:39:50 -0800

    added validation handling for login and signup routes

--- spot-me-api/routes/api/users.ts
+++ spot-me-api/routes/api/users.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import asyncHandler from 'express-async-handler';
-import { setTokenCookie, requireAuth } from '../../utils/auth';
+import { setTokenCookie } from '../../utils/auth';
 import db from '../../db/models';
+import { validateSignup } from '../../utils/validation';
 
 const router = express.Router();
 const User = db.User;
 
-router.post('/', asyncHandler(async (req: any, res: any) => {
+router.post('/', validateSignup, asyncHandler(async (req: any, res: any) => {
     const { firstName, lastName, username, email, password} = req.body;
     const user = await User.signup({ firstName, lastName, username, email, password });
 
--- spot-me-api/utils/validation.ts
+++ spot-me-api/utils/validation.ts
@@ -1,4 +1,4 @@
-import { validationResult } from "express-validator";
+import { validationResult, check } from "express-validator";
 import { RequestError } from "../app";
 
 export const handleValidationErrors = (req: any, _res: any, next: any) => {
@@ -14,3 +14,42 @@ export const handleValidationErrors = (req: any, _res: any, next: any) => {
     }
     next();
 }
+
+export const validateLogin = [
+    check('credential')
+        .exists({ checkFalsy: true })
+        .notEmpty()
+        .withMessage('Please provide a valid email or username'),
+    check('password')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a password'),
+    handleValidationErrors
+]
+
+export const validateSignup = [
+    check('firstName')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 2 })
+        .withMessage('Please provide a first name with at least 2 characters'),
+    check('lastName')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 2 })
+        .withMessage('Please provide a last name with at least 2 characters'),
+    check('email')
+        .exists({ checkFalsy: true })
+        .isEmail()
+        .withMessage('Please provide a valid email'),
+    check('username')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 2 })
+        .withMessage('Please provide a username with at least 2 characters'),
+    check('username')
+        .not()
+        .isEmail()
+        .withMessage('Username cannot be an email'),
+    check('password')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 6 })
+        .withMessage('Password must be 6 characters or more'),
+    handleValidationErrors,
+];
