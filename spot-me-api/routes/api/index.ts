310031ceb63e97feeabed474faa5283f377ab4c9
Moiz Ahmad <moiznahmad@gmail.com>
2021-11-27 09:05:16 -0800

    removed test route

--- spot-me-api/routes/api/index.ts
+++ spot-me-api/routes/api/index.ts
@@ -7,8 +7,4 @@ const router = express.Router();
 router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 
-router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body });
-});
-
 export default router;
