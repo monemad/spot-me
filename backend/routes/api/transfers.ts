47c577c79268296d31ec81065b9ee8764a86bab4
Moiz Ahmad <moiznahmad@gmail.com>
2021-12-05 09:02:51 -0800

    added error handling to post transfer

--- backend/routes/api/transfers.ts
+++ backend/routes/api/transfers.ts
@@ -3,6 +3,7 @@ import asyncHandler from 'express-async-handler';
 import db from '../../db/models';
 import { validateTransfer } from '../../utils/validation';
 import { chargeUser, payoutUser, getAvailableBalance } from './stripe';
+import { RequestError } from '../../app';
 
 const router = express.Router();
 const { Transfer, User } = db;
@@ -14,11 +15,17 @@ router.post('/', validateTransfer, asyncHandler(async (req: any, res: any) => {
     if (!deposit && (+user.balance < +amount || +(await getAvailableBalance()) < +amount) )
         return res.status(400).json({error: "Insufficient funds"})
 
-    const stripeConf = deposit ? 
-        await chargeUser(amount, `Deposit to account #${userId}`)
-        :
-        // await payoutUser(amount, `Withdrawal from account #${userId}`)
-        'paid'
+
+    let stripeConf: string | undefined = '';
+
+    try {
+        stripeConf = deposit ? 
+            await chargeUser(amount, `Deposit to account #${userId}`)
+            :
+            await payoutUser(amount, `Withdrawal from account #${userId}`)
+    } catch(e: any) {
+        return res.json(e)
+    }
 
     const transfer = await Transfer.create({
         userId,
