55442f34a28850cbb049f9224d9a3070224e8d58
Moiz Ahmad <moiznahmad@gmail.com>
2021-12-05 11:16:59 -0800

    updated README

--- backend/db/seeders/20211126093849-default-users.js
+++ backend/db/seeders/20211126093849-default-users.js
@@ -7,6 +7,13 @@ module.exports = {
         await queryInterface.bulkInsert('Users', [
             { firstName: 'Demo', lastName: 'User', username: 'demouser', email: 'demo@gmail.com', hashedPassword: await bcrypt.hash('password', 10), imgUrl: 'images/default-profile-pic.png', balance: 200.00 },
             { firstName: 'John', lastName: 'Doe', username: 'jdoe', email: 'jdoe@gmail.com', hashedPassword: await bcrypt.hash('password', 10), imgUrl: 'images/default-profile-pic.png', balance: 150.00 },
+            { firstName: 'Gary', lastName: 'Sue', username: 'gsue', email: 'gsue@gmail.com', hashedPassword: await bcrypt.hash('password', 10), imgUrl: 'images/default-profile-pic.png', balance: 150.00 },
+            { firstName: 'Rebecca', lastName: 'Flowers', username: 'rflowers', email: 'rflowers@gmail.com', hashedPassword: await bcrypt.hash('password', 10), imgUrl: 'images/default-profile-pic.png', balance: 560.00 },
+            { firstName: 'Inayat', lastName: 'Ahmad', username: 'iahmad', email: 'iahmad@gmail.com', hashedPassword: await bcrypt.hash('password', 10), imgUrl: 'images/default-profile-pic.png', balance: 234.00 },
+            { firstName: 'Christian', lastName: 'Zhang', username: 'czhang', email: 'czhang@gmail.com', hashedPassword: await bcrypt.hash('password', 10), imgUrl: 'images/default-profile-pic.png', balance: 234.00 },
+            { firstName: 'Riley', lastName: 'Adams', username: 'radams', email: 'radams@gmail.com', hashedPassword: await bcrypt.hash('password', 10), imgUrl: 'images/default-profile-pic.png', balance: 15044.00 },
+            { firstName: 'Moiz', lastName: 'Ahmad', username: 'wizkika', email: 'therealwizkika@gmail.com', hashedPassword: await bcrypt.hash('password', 10), imgUrl: 'images/default-profile-pic.png', balance: 150345.00 },
+            { firstName: 'Mayer', lastName: 'Hawthonre', username: 'mhawthorne', email: 'mhawthorne@gmail.com', hashedPassword: await bcrypt.hash('password', 10), imgUrl: 'images/default-profile-pic.png', balance: 1504432.00 },
             { firstName: 'Khalil', lastName: 'Gibran', username: 'kgibran', email: 'k.gibran@gmail.com', hashedPassword: await bcrypt.hash('password', 10), imgUrl: 'images/default-profile-pic.png', balance: 9500.69 },
         ], {})
     },
--- backend/db/seeders/20211128180745-default-payments.js
+++ backend/db/seeders/20211128180745-default-payments.js
@@ -4,6 +4,10 @@ module.exports = {
     up: async (queryInterface, Sequelize) => {
         await queryInterface.bulkInsert('Payments', [
             {senderId: 1, recipientId: 2, amount: 20.00, memo: "Dinner last night", fulfilled: true},
+            {senderId: 1, recipientId: 7, amount: 20.00, memo: "gamezz", fulfilled: true},
+            {senderId: 1, recipientId: 8, amount: 560.00, memo: "rent money", fulfilled: true},
+            {senderId: 1, recipientId: 4, amount: 40.00, memo: "happy hour", fulfilled: true},
+            {senderId: 1, recipientId: 4, amount: 7.00, memo: "for fun", fulfilled: false},
             {senderId: 2, recipientId: 1, amount: 10.00, memo: "Gas money", fulfilled: false},
             {senderId: 2, recipientId: 1, amount: 5.00, memo: "toilet paper", fulfilled: true},
         ], {})
